90a7dc83fe482b69d90f719916ff0a81e7bf8230
lucas Tano <lucasgabriel.tano@gmail.com>
2024-04-23 23:28:50 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoService.LogicaNegocio/Modelo/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoService.LogicaNegocio.Modelo
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+
+        public string  Modelo { get; set; }
+
+
+    }
+}
